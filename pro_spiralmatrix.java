139d8fc0ae6f2ee4698cec2b4a62f25746061ee1
shivkumar udas <shivkumarudas07@gmail.com>
2026-01-09 23:14:04 +0530

    push

---
+++ pro_spiralmatrix.java
@@ -0,0 +1,42 @@
+public class pro_spiralmatrix {
+    public static void Sprialmat(int matrix[][]){
+        int n=matrix.length;
+        int m=matrix[0].length;
+        int top=0;
+        int bottom=n-1;
+        int left=0;
+        int right=m-1;
+        while(top<=bottom && left<=right){
+            for(int i=left;i<=right;i++){
+                System.out.print(matrix[top][i]+" ");
+            }
+            top++;
+            for(int i=top;i<=bottom;i++){
+                System.out.print(matrix[i][right]+" ");
+            }
+            right--;
+            if(top<=bottom){
+                for(int i=right;i>=left;i--){
+                    System.out.print(matrix[bottom][i]+" ");
+                }
+                bottom--; 
+            }
+            if(left<=right){
+                for(int i=bottom;i>=top;i--){
+                    System.out.print(matrix[i][left]+" ");
+                }
+                left++;
+        }
+    }
+    System.out.println();
+
+    }
+    public static void main(String[] args){
+        int matrix[][]={{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
+        Sprialmat(matrix);
+  
+
+    }
+
+}
+
