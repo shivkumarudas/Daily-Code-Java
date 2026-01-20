c21239124e6e59dc3bd835d1cf5a5d9aeff7d5ca
shivkumar udas <shivkumarudas07@gmail.com>
2026-01-20 22:36:00 +0530

    Create pro_substring.java

---
+++ pro_substring.java
@@ -0,0 +1,7 @@
+public class pro_substring {
+    public static void main(String[] args) {
+        String s = "HelloWorld";
+        String sub = s.substring(0, 5); // Extracts "Hello"
+        System.out.println(sub);
+    }
+}
