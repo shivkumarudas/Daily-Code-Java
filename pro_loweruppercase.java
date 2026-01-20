8b3644fc6041fb0c787318a2e42c9d20d19f278a
shivkumar udas <shivkumarudas07@gmail.com>
2026-01-20 22:36:18 +0530

    Create pro_loweruppercase.java

---
+++ pro_loweruppercase.java
@@ -0,0 +1,11 @@
+public class pro_loweruppercase {
+    public static void main(String[] args) {
+        String str = "Hello World";
+        String lowerStr = str.toLowerCase();
+        String upperStr = str.toUpperCase();
+        
+        System.out.println("Original String: " + str);
+        System.out.println("Lowercase String: " + lowerStr);
+        System.out.println("Uppercase String: " + upperStr);
+    }
+}
