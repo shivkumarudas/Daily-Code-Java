50406574f57b10b21a3dd12a46c64d03eda92e2a
shivkumar udas <shivkumarudas07@gmail.com>
2026-01-20 22:36:21 +0530

    Create pro_stringcharAt.java

---
+++ pro_stringcharAt.java
@@ -0,0 +1,7 @@
+public class pro_stringcharAt {
+    public static void main(String[] args) {
+        String s = "Java";
+        System.out.println(s.charAt(0)); // J
+        System.out.println(s.charAt(3)); // a
+    }
+}
