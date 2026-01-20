730d76e3f4e0c84d77b2c429fe8f7670ca38e5f3
shivkumar udas <shivkumarudas07@gmail.com>
2026-01-20 22:36:07 +0530

    Create pro_contains.java

---
+++ pro_contains.java
@@ -0,0 +1,7 @@
+public class pro_contains {
+    public static void main(String[] args) {
+        String s = "Hello World";
+        System.out.println(s.contains("World")); // true
+        System.out.println(s.contains("Java"));  // false
+    }
+}
