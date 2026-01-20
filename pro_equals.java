add5935c33ae498a95b28140b18986482865d848
shivkumar udas <shivkumarudas07@gmail.com>
2026-01-20 22:36:11 +0530

    Create pro_equals.java

---
+++ pro_equals.java
@@ -0,0 +1,10 @@
+public class pro_equals {
+    public static void main(String[] args) {
+        String str1 = "Hello";
+        String str2 = "Hello";
+        String str3 = "World";
+        
+        System.out.println("str1 equals str2: " + str1.equals(str2)); // true
+        System.out.println("str1 equals str3: " + str1.equals(str3)); // false
+    }
+}
