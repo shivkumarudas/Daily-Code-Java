c5cd587b9efa7fca52dbaae094eabc4504109dd6
shivkumar udas <shivkumarudas07@gmail.com>
2026-01-21 23:33:53 +0530

    Create pro_replace.java

---
+++ pro_replace.java
@@ -0,0 +1,8 @@
+public class pro_replace {
+    
+    public static void main(String[] args) {
+        String s = "I love Java";
+
+        System.out.println(s.replace("Java", "Python"));
+    }
+}
