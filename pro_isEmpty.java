b768dd2182e82af8f8d9cc7fb94a7863651552c1
shivkumar udas <shivkumarudas07@gmail.com>
2026-01-21 23:33:39 +0530

    Create pro_isEmpty.java

---
+++ pro_isEmpty.java
@@ -0,0 +1,9 @@
+public class pro_isEmpty {
+    public static void main(String[] args) {
+        String s1 = "";
+        String s2 = "Hello";
+
+        System.out.println(s1.isEmpty()); // true
+        System.out.println(s2.isEmpty()); // false
+    }
+}
