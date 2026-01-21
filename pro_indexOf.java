d9693a6d503a4892d67781dd70704c10e2677611
shivkumar udas <shivkumarudas07@gmail.com>
2026-01-21 23:33:36 +0530

    Create pro_indexOf.java

---
+++ pro_indexOf.java
@@ -0,0 +1,7 @@
+public class pro_indexOf {
+    public static void main(String[] args) {
+        String s = "Hello World";
+        System.out.println(s.indexOf("World")); // 6
+        System.out.println(s.indexOf("Java"));  // -1
+    }
+}
