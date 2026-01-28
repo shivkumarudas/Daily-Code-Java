fa3f679d6ea3bd795c54d007a0480eb6355f5faa
shivkumar udas <shivkumarudas07@gmail.com>
2026-01-28 23:39:01 +0530

    Create pro_PalindromeString.java

---
+++ pro_PalindromeString.java
@@ -0,0 +1,16 @@
+public class pro_PalindromeString {
+ 
+    public static void main(String[] args) {
+        String s = "madam";
+        String rev = "";
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            rev += s.charAt(i);
+        }
+
+        if (s.equals(rev))
+            System.out.println("Palindrome");
+        else
+            System.out.println("Not Palindrome");
+    }
+}
