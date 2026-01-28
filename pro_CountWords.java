2b80b883d3f7efa96ed0c1cf20e1a4bdcca3df33
shivkumar udas <shivkumarudas07@gmail.com>
2026-01-28 23:39:04 +0530

    Create pro_CountWords.java

---
+++ pro_CountWords.java
@@ -0,0 +1,8 @@
+public class pro_CountWords {
+    public static void main(String[] args) {
+        String s = "Java is very powerful";
+        String[] words = s.split(" ");
+
+        System.out.println(words.length);
+    }
+}
