3972ff641cae73745c922ecfb2a4ee3291a6c0f7
shivkumar udas <shivkumarudas07@gmail.com>
2026-02-05 23:39:27 +0530

    Create pro_reverselinkedlist.java

---
+++ pro_reverselinkedlist.java
@@ -0,0 +1,31 @@
+public class pro_reverselinkedlist {
+
+    static class Node {
+        int data;
+        Node next;
+        Node(int d) { data = d; }
+    }
+
+    static Node reverse(Node head) {
+        Node prev = null, curr = head;
+        while (curr != null) {
+            Node next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+
+    public static void main(String[] args) {
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+
+        head = reverse(head);
+        while (head != null) {
+            System.out.print(head.data + " ");
+            head = head.next;
+        }
+    }
+}
