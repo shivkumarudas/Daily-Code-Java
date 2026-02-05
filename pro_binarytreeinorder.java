3f973de0aac729a9d948ba9440e6492669e60576
shivkumar udas <shivkumarudas07@gmail.com>
2026-02-05 23:39:53 +0530

    Create pro_binarytreeinorder.java

---
+++ pro_binarytreeinorder.java
@@ -0,0 +1,21 @@
+class Node {
+    int data;
+    Node left, right;
+    Node(int d) { data = d; }
+}
+
+public class pro_binarytreeinorder {
+    static void inorder(Node root) {
+        if (root == null) return;
+        inorder(root.left);
+        System.out.print(root.data + " ");
+        inorder(root.right);
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        inorder(root);
+    }
+}
