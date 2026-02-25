2802f0e5478c086979c345b09fbc7a4dbf48c09d
shivkumar udas <shivkumarudas07@gmail.com>
2026-02-25 23:58:56 +0530

    Create pro_armstrong.java

---
+++ pro_armstrong.java
@@ -0,0 +1,19 @@
+public class pro_armstrong {
+    public static void main(String[] args) {
+        int num = 153;
+        int original = num;
+        int sum = 0;
+
+        while (num > 0) {
+            int digit = num % 10;
+            sum += digit * digit * digit;
+            num /= 10;
+        }
+
+        if (sum == original) {
+            System.out.println(original + " is an Armstrong number");
+        } else {
+            System.out.println(original + " is not an Armstrong number");
+        }
+    }
+}
