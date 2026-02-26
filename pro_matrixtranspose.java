7ac9a0f17846760285699d6525bfe8ccd07a5fa3
shivkumar udas <shivkumarudas07@gmail.com>
2026-02-26 23:01:12 +0530

    Create pro_matrixtranspose.java

---
+++ pro_matrixtranspose.java
@@ -0,0 +1,26 @@
+public class pro_matrixtranspose {
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6}
+        };
+
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int[][] transpose = new int[cols][rows];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                transpose[j][i] = matrix[i][j];
+            }
+        }
+
+        System.out.println("Transpose matrix:");
+        for (int i = 0; i < transpose.length; i++) {
+            for (int j = 0; j < transpose[i].length; j++) {
+                System.out.print(transpose[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
