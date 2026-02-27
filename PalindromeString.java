a0aa1583b7cf4664bb08ece13380c132c754483f
shivkumar udas <shivkumarudas07@gmail.com>
2026-02-27 22:18:18 +0530

    Create PalindromeString.java

---
+++ PalindromeString.java
@@ -0,0 +1,7 @@
+public class PalindromeString {
+    public static void main(String[] args) {
+        String s = "madam";
+        String rev = new StringBuilder(s).reverse().toString();
+        System.out.println(s.equals(rev) ? "Palindrome" : "Not Palindrome");
+    }
+}
