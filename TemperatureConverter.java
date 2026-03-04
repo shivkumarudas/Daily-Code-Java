0257093a73bdef8f289f22e411d5b3dde8a0cce2
shivkumar udas <shivkumarudas07@gmail.com>
2026-03-04 22:53:10 +0530

    Create TemperatureConverter.java

---
+++ TemperatureConverter.java
@@ -0,0 +1,21 @@
+// Program for temperature conversion
+public class TemperatureConverter {
+    
+    static double celsiusToFahrenheit(double celsius) {
+        return (celsius * 9/5) + 32;
+    }
+    
+    static double fahrenheitToCelsius(double fahrenheit) {
+        return (fahrenheit - 32) * 5/9;
+    }
+    
+    public static void main(String[] args) {
+        double celsius = 25;
+        double fahrenheit = 77;
+        
+        System.out.println("Temperature Conversion");
+        System.out.println("======================");
+        System.out.println(celsius + "°C = " + celsiusToFahrenheit(celsius) + "°F");
+        System.out.println(fahrenheit + "°F = " + fahrenheitToCelsius(fahrenheit) + "°C");
+    }
+}
