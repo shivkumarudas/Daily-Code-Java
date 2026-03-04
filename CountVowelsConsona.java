481f23d944e742c024002ad4c0c55a072560e9c9
shivkumar udas <shivkumarudas07@gmail.com>
2026-03-04 22:53:39 +0530

    Create CountVowelsConsona.java

---
+++ CountVowelsConsona.java
@@ -0,0 +1,24 @@
+// Program to count vowels and consonants
+public class CountVowelsConsona {
+    public static void main(String[] args) {
+        String str = "Java Programming".toLowerCase();
+        int vowels = 0, consonants = 0;
+        
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            
+            if (ch >= 'a' && ch <= 'z') {
+                if (ch == 'a' || ch == 'e' || ch == 'i' || 
+                    ch == 'o' || ch == 'u') {
+                    vowels++;
+                } else {
+                    consonants++;
+                }
+            }
+        }
+        
+        System.out.println("String: " + str);
+        System.out.println("Vowels: " + vowels);
+        System.out.println("Consonants: " + consonants);
+    }
+}
