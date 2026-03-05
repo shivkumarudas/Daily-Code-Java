ec400a68aa3209a45c1744a0fd12c336451c33bc
shivkumar udas <shivkumarudas07@gmail.com>
2026-03-05 22:30:26 +0530

    Create max.java

---
+++ max.java
@@ -0,0 +1,13 @@
+public class max {
+    public static void main(String[] args) {
+        int [] arr=new int[]{1,2,3,4,5};
+        int max=0;
+        System.out.println("Original array:");
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>max){
+                max=arr[i];
+            }
+        }
+        System.out.println("Maximum element in the array is: "+max);
+    }
+}
