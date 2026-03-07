3a01bbd070da21d3e483152286e1a8ba64edc2d5
shivkumar udas <shivkumarudas07@gmail.com>
2026-03-07 19:38:17 +0530

    Create Program14LCM.java

---
+++ Program14LCM.java
@@ -0,0 +1,19 @@
+class Program14LCM {
+    public static void main(String[] args) {
+        int a = 12;
+        int b = 18;
+        int x = a;
+        int y = b;
+
+        while (y != 0) {
+            int temp = y;
+            y = x % y;
+            x = temp;
+        }
+
+        int gcd = x;
+        int lcm = (a * b) / gcd;
+
+        System.out.println("LCM = " + lcm);
+    }
+}
