1375e3893da992d05fa739d8e568788b5107e45b
shivkumar udas <shivkumarudas07@gmail.com>
2026-03-07 19:38:10 +0530

    Create Program13GCD.java

---
+++ Program13GCD.java
@@ -0,0 +1,14 @@
+class Program13GCD {
+    public static void main(String[] args) {
+        int a = 48;
+        int b = 18;
+
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+
+        System.out.println("GCD = " + a);
+    }
+}
