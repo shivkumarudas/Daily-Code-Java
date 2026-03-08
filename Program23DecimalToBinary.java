e64796595ca53974dc70a384673b25ebe1be86b5
shivkumar udas <shivkumarudas07@gmail.com>
2026-03-08 23:26:54 +0530

    Create Program23DecimalToBinary.java

---
+++ Program23DecimalToBinary.java
@@ -0,0 +1,8 @@
+public class Program23DecimalToBinary {
+    public static void main(String[] args) {
+        int number = 25;
+        String binary = Integer.toBinaryString(number);
+        System.out.println("Decimal: " + number);
+        System.out.println("Binary: " + binary);
+    }
+}
