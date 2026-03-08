297b4dc9277689379a201cf9c503ff2af2452da5
shivkumar udas <shivkumarudas07@gmail.com>
2026-03-08 23:27:17 +0530

    Create Program29SumOfNaturalNumbers.java

---
+++ Program29SumOfNaturalNumbers.java
@@ -0,0 +1,7 @@
+public class Program29SumOfNaturalNumbers {
+    public static void main(String[] args) {
+        int n = 100;
+        int sum = n * (n + 1) / 2;
+        System.out.println("Sum of first " + n + " natural numbers is: " + sum);
+    }
+}
