a5529f8425ed24e2471d3b80dff9a1d42a4bbea0
shivkumar udas <shivkumarudas07@gmail.com>
2026-03-08 23:24:01 +0530

    Update max.java

--- max.java
+++ max.java
@@ -1,6 +1,6 @@
 public class max {
     public static void main(String[] args) {
-        int [] arr=new int[]{1,2,3,4,5};
+        int [] arr=new int[]{1,2,3,4,5,10};
         int max=0;
         System.out.println("Original array:");
         for(int i=0;i<arr.length;i++){
