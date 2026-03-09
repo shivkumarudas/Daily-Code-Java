ad60b08735a9bc392eaa9a3e03b9a0f78389d4f2
shivkumar udas <shivkumarudas07@gmail.com>
2026-03-09 21:29:25 +0530

    Create Program30LeapYearCheck.java

---
+++ Program30LeapYearCheck.java
@@ -0,0 +1,12 @@
+public class Program30LeapYearCheck {
+    public static void main(String[] args) {
+        int year = 2024;
+        boolean isLeap = (year % 400 == 0) || (year % 4 == 0 && year % 100 != 0);
+
+        if (isLeap) {
+            System.out.println(year + " is a leap year");
+        } else {
+            System.out.println(year + " is not a leap year");
+        }
+    }
+}
