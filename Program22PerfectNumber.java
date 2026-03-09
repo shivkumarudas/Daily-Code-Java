d0322eacce715161e252004a90479f854f1d0454
shivkumar udas <shivkumarudas07@gmail.com>
2026-03-09 21:29:53 +0530

    Create Program22PerfectNumber.java

---
+++ Program22PerfectNumber.java
@@ -0,0 +1,18 @@
+public class Program22PerfectNumber {
+    public static void main(String[] args) {
+        int number = 28;
+        int sum = 0;
+
+        for (int i = 1; i <= number / 2; i++) {
+            if (number % i == 0) {
+                sum += i;
+            }
+        }
+
+        if (sum == number) {
+            System.out.println(number + " is a perfect number");
+        } else {
+            System.out.println(number + " is not a perfect number");
+        }
+    }
+}
