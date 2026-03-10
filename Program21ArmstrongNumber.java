9dfac44e833946afc2bf9c8c0ea2b2be4025bdba
shivkumar udas <shivkumarudas07@gmail.com>
2026-03-10 22:51:31 +0530

    Create Program21ArmstrongNumber.java

---
+++ Program21ArmstrongNumber.java
@@ -0,0 +1,19 @@
+public class Program21ArmstrongNumber {
+    public static void main(String[] args) {
+        int number = 153;
+        int temp = number;
+        int sum = 0;
+
+        while (temp > 0) {
+            int digit = temp % 10;
+            sum += digit * digit * digit;
+            temp /= 10;
+        }
+
+        if (sum == number) {
+            System.out.println(number + " is an Armstrong number");
+        } else {
+            System.out.println(number + " is not an Armstrong number");
+        }
+    }
+}
