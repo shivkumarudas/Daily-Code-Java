bc97ed542d762a82466c2c22ae97e06d6126a999
shivkumar udas <shivkumarudas07@gmail.com>
2026-03-11 23:32:36 +0530

    Create Function01AddNumbers.java

---
+++ Function01AddNumbers.java
@@ -0,0 +1,11 @@
+public class Function01AddNumbers {
+    static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        int first = 15;
+        int second = 25;
+        System.out.println("Sum = " + add(first, second));
+    }
+}
