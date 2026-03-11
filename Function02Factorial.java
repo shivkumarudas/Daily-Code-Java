8b79072aed3d920aa1fc9b5bb5b62f8d6a19bcac
shivkumar udas <shivkumarudas07@gmail.com>
2026-03-11 23:33:56 +0530

    Create Function02Factorial.java

---
+++ Function02Factorial.java
@@ -0,0 +1,14 @@
+public class Function02Factorial {
+    static long factorial(int n) {
+        long result = 1;
+        for (int i = 2; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int number = 6;
+        System.out.println("Factorial of " + number + " = " + factorial(number));
+    }
+}
