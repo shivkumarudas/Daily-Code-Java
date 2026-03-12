7841e0998f35bb53fca46dac0bc2b1a0aedf7013
shivkumar udas <shivkumarudas07@gmail.com>
2026-03-12 22:23:22 +0530

    Create Program40AverageOfNNumbers.java

---
+++ Program40AverageOfNNumbers.java
@@ -0,0 +1,13 @@
+public class Program40AverageOfNNumbers {
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 30, 40, 50};
+        int sum = 0;
+
+        for (int num : numbers) {
+            sum += num;
+        }
+
+        double average = (double) sum / numbers.length;
+        System.out.println("Average: " + average);
+    }
+}
