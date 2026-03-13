818ab9a0345f5186c75ef1dfc0d94c33cf143e9b
shivkumar udas <shivkumarudas07@gmail.com>
2026-03-13 22:19:58 +0530

    Create Program31MultiplicationTable.java

---
+++ Program31MultiplicationTable.java
@@ -0,0 +1,10 @@
+public class Program31MultiplicationTable {
+    public static void main(String[] args) {
+        int number = 7;
+
+        System.out.println("Multiplication table of " + number + ":");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(number + " x " + i + " = " + (number * i));
+        }
+    }
+}
