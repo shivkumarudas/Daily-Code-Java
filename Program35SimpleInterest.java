bb90dd04a3af36a5f8bc622af0f8853e498fde58
shivkumar udas <shivkumarudas07@gmail.com>
2026-03-13 22:19:29 +0530

    Create Program35SimpleInterest.java

---
+++ Program35SimpleInterest.java
@@ -0,0 +1,11 @@
+public class Program35SimpleInterest {
+    public static void main(String[] args) {
+        double principal = 5000;
+        double rate = 7.5;
+        double time = 3;
+
+        double simpleInterest = (principal * rate * time) / 100;
+
+        System.out.println("Simple Interest: " + simpleInterest);
+    }
+}
