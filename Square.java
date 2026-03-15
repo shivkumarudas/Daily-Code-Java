3ac525f1e1bb8099f3bedbe98b347a02bbf20791
shivkumar udas <shivkumarudas07@gmail.com>
2026-03-15 23:27:28 +0530

    Create Square.java

---
+++ Square.java
@@ -0,0 +1,7 @@
+public class Square {
+    public static void main(String[] args) {
+        int num = 6;
+        int square = num * num;
+        System.out.println("Square: " + square);
+    }
+}
