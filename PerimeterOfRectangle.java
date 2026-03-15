66a28e3e1f93f11f25ec83af36be483636b27578
shivkumar udas <shivkumarudas07@gmail.com>
2026-03-15 23:27:33 +0530

    Create PerimeterOfRectangle.java

---
+++ PerimeterOfRectangle.java
@@ -0,0 +1,7 @@
+public class PerimeterOfRectangle {
+    public static void main(String[] args) {
+        int length = 10, width = 5;
+        int perimeter = 2 * (length + width);
+        System.out.println("Perimeter of rectangle: " + perimeter);
+    }
+}
