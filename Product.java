02a4cccf1a7f71f394f75a367b9cdb47f5fad49d
shivkumar udas <shivkumarudas07@gmail.com>
2026-03-15 23:27:30 +0530

    Create Product.java

---
+++ Product.java
@@ -0,0 +1,8 @@
+public class Product {
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 7;
+        int product = a * b;
+        System.out.println("Product: " + product);
+    }
+}
