24f1da2612407582350b139060e859f6a2aaaa50
shivkumar udas <shivkumarudas07@gmail.com>
2026-03-17 23:15:00 +0530

    Create Program45MergeTwoSortedArrays.java

---
+++ Program45MergeTwoSortedArrays.java
@@ -0,0 +1,33 @@
+public class Program45MergeTwoSortedArrays {
+    public static void main(String[] args) {
+        int[] arr1 = {1, 3, 5, 7};
+        int[] arr2 = {2, 4, 6, 8};
+        int[] merged = new int[arr1.length + arr2.length];
+
+        int i = 0;
+        int j = 0;
+        int k = 0;
+
+        while (i < arr1.length && j < arr2.length) {
+            if (arr1[i] < arr2[j]) {
+                merged[k++] = arr1[i++];
+            } else {
+                merged[k++] = arr2[j++];
+            }
+        }
+
+        while (i < arr1.length) {
+            merged[k++] = arr1[i++];
+        }
+
+        while (j < arr2.length) {
+            merged[k++] = arr2[j++];
+        }
+
+        System.out.print("Merged array: ");
+        for (int num : merged) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+}
