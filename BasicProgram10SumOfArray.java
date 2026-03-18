b973aa67b3eb3c4deed69fc92f0b4b3ba37ef2e7
shivkumar udas <shivkumarudas07@gmail.com>
2026-03-18 23:07:33 +0530

    Create BasicProgram10SumOfArray.java

---
+++ BasicProgram10SumOfArray.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class BasicProgram10SumOfArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += sc.nextInt();
+        }
+        System.out.println("Sum = " + sum);
+        sc.close();
+    }
+}
