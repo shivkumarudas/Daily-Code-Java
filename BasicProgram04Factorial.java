54af356c05bcda6719ed3c038a5f162c4a7c4eec
shivkumar udas <shivkumarudas07@gmail.com>
2026-03-19 21:31:39 +0530

    Create BasicProgram04Factorial.java

---
+++ BasicProgram04Factorial.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class BasicProgram04Factorial {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        long fact = 1;
+        for (int i = 1; i <= n; i++) {
+            fact *= i;
+        }
+        System.out.println("Factorial = " + fact);
+        sc.close();
+    }
+}
