d016ee22a64ef1b9b6dc6e4ec9e569b5f74f86c8
shivkumar udas <shivkumarudas07@gmail.com>
2026-03-21 23:17:05 +0530

    Create pro_checkprime.java

---
+++ pro_checkprime.java
@@ -0,0 +1,19 @@
+public class pro_checkprime {
+    static boolean isPrime(int n) {
+        if (n <= 1) return false;
+        if (n <= 3) return true;
+        if (n % 2 == 0 || n % 3 == 0) return false;
+        
+        for (int i = 5; i * i <= n; i += 6) {
+            if (n % i == 0 || n % (i + 2) == 0) return false;
+        }
+        return true;
+    }
+    
+    public static void main(String[] args) {
+        int[] numbers = {2, 3, 4, 17, 20, 29};
+        for (int num : numbers) {
+            System.out.println(num + " is " + (isPrime(num) ? "Prime" : "Not Prime"));
+        }
+    }
+}
