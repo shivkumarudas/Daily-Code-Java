f8e6e61e46e54865ecb20500d482ac32cc243252
shivkumar udas <shivkumarudas07@gmail.com>
2026-03-27 22:56:34 +0530

    Create Program6.java

---
+++ JavaPrograms/Program6.java
@@ -0,0 +1,13 @@
+public class Program6 {
+    public static void main(String[] args) {
+        int n = 10;
+        int a = 0, b = 1;
+        System.out.print("Fibonacci: " + a + " " + b);
+        for (int i = 2; i < n; i++) {
+            int next = a + b;
+            System.out.print(" " + next);
+            a = b;
+            b = next;
+        }
+    }
+}
