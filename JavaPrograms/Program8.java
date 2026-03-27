fe62816e423fdb3992130f710ff0547a3d8dc2ed
shivkumar udas <shivkumarudas07@gmail.com>
2026-03-27 22:56:18 +0530

    Create Program8.java

---
+++ JavaPrograms/Program8.java
@@ -0,0 +1,11 @@
+public class Program8 {
+    public static void main(String[] args) {
+        String str = "radar";
+        String reversed = new StringBuilder(str).reverse().toString();
+        if (str.equals(reversed)) {
+            System.out.println(str + " is a palindrome");
+        } else {
+            System.out.println(str + " is not a palindrome");
+        }
+    }
+}
