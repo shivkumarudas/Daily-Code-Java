b95d16554a3207835eeba0ba4767a6bbc71080aa
shivkumar udas <shivkumarudas07@gmail.com>
2026-03-28 23:05:06 +0530

    Create Program10.java

---
+++ JavaPrograms/Program10.java
@@ -0,0 +1,10 @@
+public class Program10 {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        int sum = 0;
+        for (int num : arr) {
+            sum += num;
+        }
+        System.out.println("Sum of array: " + sum);
+    }
+}
