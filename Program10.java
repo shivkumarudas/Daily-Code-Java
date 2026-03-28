7ee69e4a40fd25f6d24752f102fdecdfa7c7ffab
shivkumar udas <shivkumarudas07@gmail.com>
2026-03-28 23:04:40 +0530

    Create Program10.java

---
+++ Program10.java
@@ -0,0 +1,14 @@
+public class Program10 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Program10!");
+        int x = 10;
+        int y = 20;
+        int result = x + y;
+        System.out.println("Addition result: " + result);
+        for (int i = 1; i <= 3; i++) {
+            System.out.println("Iteration: " + i);
+        }
+        String greeting = "Thank you for running this program.";
+        System.out.println(greeting);
+    }
+}
