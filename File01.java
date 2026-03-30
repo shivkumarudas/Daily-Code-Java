a00d6895802c39a8835869e7c9765df740d4f845
shivkumar udas <shivkumarudas07@gmail.com>
2026-03-30 22:47:58 +0530

    Create File01.java

---
+++ File01.java
@@ -0,0 +1,14 @@
+public class File01 {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        int a = 5;
+        int b = 10;
+        int sum = a + b;
+        System.out.println("Sum: " + sum);
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Count: " + i);
+        }
+        String message = "This is a simple Java program.";
+        System.out.println(message);
+    }
+}
