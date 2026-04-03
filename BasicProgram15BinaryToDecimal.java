a1507bfdd2379cd0b3f2dea7d187a269717838e0
shivkumar udas <shivkumarudas07@gmail.com>
2026-04-03 22:09:03 +0530

    Create BasicProgram15BinaryToDecimal.java

---
+++ BasicProgram15BinaryToDecimal.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class BasicProgram15BinaryToDecimal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a binary number: ");
+        String binary = scanner.nextLine();
+        int decimal = Integer.parseInt(binary, 2);
+        System.out.println("Decimal equivalent: " + decimal);
+        scanner.close();
+    }
+}
