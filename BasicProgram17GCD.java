b43b6233a727a645860eaf4394ae7fb10451fec7
shivkumar udas <shivkumarudas07@gmail.com>
2026-04-03 22:08:58 +0530

    Create BasicProgram17GCD.java

---
+++ BasicProgram17GCD.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class BasicProgram17GCD {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int a = scanner.nextInt();
+        System.out.print("Enter second number: ");
+        int b = scanner.nextInt();
+        int gcd = findGCD(a, b);
+        System.out.println("GCD of " + a + " and " + b + " is: " + gcd);
+        scanner.close();
+    }
+
+    public static int findGCD(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+}
