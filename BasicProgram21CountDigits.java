bc4139ed6831479edf78f1d38e64701c50b1bcac
shivkumar udas <shivkumarudas07@gmail.com>
2026-04-06 01:25:02 +0530

    Create BasicProgram21CountDigits.java

---
+++ BasicProgram21CountDigits.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class BasicProgram21CountDigits {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter an integer: ");
+        int number = scanner.nextInt();
+
+        int temp = Math.abs(number);
+        int count = (temp == 0) ? 1 : 0;
+
+        while (temp > 0) {
+            count++;
+            temp /= 10;
+        }
+
+        System.out.println("Number of digits: " + count);
+        scanner.close();
+    }
+}
